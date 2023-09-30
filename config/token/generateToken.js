7766de301029747844a895cb102868aac34439d5
001Aakriti <aakreteeshah157@gmail.com>
2023-09-30 08:11:51 +0530

    first commit

---
+++ config/token/generateToken.js
@@ -0,0 +1,9 @@
+require('dotenv').config();
+
+const jwt = require("jsonwebtoken");
+
+const generateToken = id => {
+  return jwt.sign({ id }, process.env.JWT_KEY, { expiresIn: "20d" });
+};
+
+module.exports = generateToken;
